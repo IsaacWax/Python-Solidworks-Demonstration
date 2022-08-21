293c0ab9bc387b2893806463c1d0d4dbcbbff786
Isaac <isaacggwax@gmail.com>
2022-08-21 15:15:39 -0700

    added read functon

--- python3-solidworks.py
+++ python3-solidworks.py
@@ -1,20 +1,17 @@
-import math
-#Written by Isaac Wax as a simple demmo on how you can use Solidworks do equation driven modeling from a text file
-
-#Open files to input variables
-f = open('boss.txt', 'w+')
-
-#User input
-#Assign input values...
 width = input('Width: ')
 length = input('Lenght: ')
 height = input('Height: ')
 
+#Open files to input variables
+f = open("boss.txt", "w+")
 #Wright to .txt file
 #Place user input in .txt
 f.write('width = '+width + '\n')
-f.write('width = '+length + '\n')
-f.write('width = '+height + '\n')
+f.write('length = '+length + '\n')
+f.write('height = '+height + '\n')
 
-#Close and save .txt
 f.close()
+
+#open and read the file after the appending:
+f = open("boss.txt", "r")
+print(f.read())
