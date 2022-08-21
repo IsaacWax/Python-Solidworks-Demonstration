5e803da450f582764954f9d6dc7c265461e4dbdc
Isaac <isaacggwax@gmail.com>
2022-08-21 13:52:25 -0700

    add inputs

--- python3-solidworks.py
+++ python3-solidworks.py
@@ -1,1 +1,20 @@
 import math
+#Written by Isaac Wax as a simple demmo on how you can use Solidworks do equation driven modeling from a text file
+
+#Open files to input variables
+f = open('boss.txt', 'w+')
+
+#User input
+#Assign input values...
+width = input('Width: ')
+length = input('Lenght: ')
+height = input('Height: ')
+
+#Wright to .txt file
+#Place user input in .txt
+f.write('width = '+width + '\n')
+f.write('width = '+length + '\n')
+f.write('width = '+height + '\n')
+
+#Close and save .txt
+f.close()
